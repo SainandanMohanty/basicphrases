306630bd7c53272eeacf1d410f77517907b8add6
Sainandan Mohanty <sainandan.mohanty.sm@gmail.com>
2018-05-31 15:22:04 +0530

    Implement MediaPlayer

--- app/src/main/java/com/example/sain/basicphrases/MainActivity.java
+++ app/src/main/java/com/example/sain/basicphrases/MainActivity.java
@@ -4,6 +4,7 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,11 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 
     public void onClick(View view) {
+        Button button = (Button) view;
+        String filename = button.getText().toString().replace(" ", "_");
 
+        int resId = getResources().getIdentifier(filename, "raw", getPackageName());
+
+        mediaPlayer = MediaPlayer.create(this, resId);
+        mediaPlayer.start();
     }
 }
